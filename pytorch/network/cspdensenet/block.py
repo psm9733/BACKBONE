7fed72c9bd820ee9008c5bc588a499a865e4b0c8
SangMin Park <psm9733@naver.com>
2021-09-27 13:49:48 +0900

    Remove : cspdensenet

--- pytorch/network/cspdensenet/block.py
+++
@@ -1,4 +0,0 @@
-import torch
-import torch.nn as nn
-from network.common.layers import Conv2D_BN
-import torch.nn as nn
--- pytorch/network/cspdensenet/cspdensenet.py
+++
@@ -1,7 +0,0 @@
-from network.common.layers import *
-from network.densenet.blocks import *
-import torch.nn as nn
-
-class CspDenseNetMicro(nn.Module):
-    def __init__(self, activation, bias=True):
-        super(CspDenseNetMicro, self).__init__()
